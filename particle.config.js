e12dfa5b05f3da75a4f766dca2b82c9edd88f861
bimm-cpu <afnierangkuti@gmail.com>
2026-04-22 18:38:22 +0700

    Create particle.config.js

---
+++ particle.config.js
@@ -0,0 +1,31 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 120
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      enable: true,
+      speed: 0.5
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+});
